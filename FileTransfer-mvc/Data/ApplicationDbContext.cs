b7896c6aa18516e03a8252e13a4d7ce0702aa171
amitmanna001 <amitmanna816@gmail.com>
2025-11-22 20:40:03 +0530

    feat: configure database connection and add User entity DbSet

---
+++ FileTransfer-mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using FileTransfer_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileTransfer_mvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ FileTransfer-mvc/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FileTransfer_mvc.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string FolderPath { get; set; }
+    }
+}
